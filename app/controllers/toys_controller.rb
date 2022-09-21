70d3f4bd2901f5d163448df9528903916ba09a98
Nathan <namo678@me.com>
2022-09-21 17:29:35 -0400

    commit tree 7bc327b4f13b5a2cdc3e0802ddb2c17750cde858

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,19 +1,23 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
 
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable
+
   def index
     toys = Toy.all
     render json: toys
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+    render json: toy, status: 202
   end
 
   def destroy
@@ -26,6 +30,14 @@ class ToysController < ApplicationController
   
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def render_not_found
+    render json: {error: 'Toy not found'}, status: :not_found
+  end
+
+  def render_unprocessable(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
   end
 
 end
